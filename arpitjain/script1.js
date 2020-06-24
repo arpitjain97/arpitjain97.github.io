36ac611e62913b29e135e67f1bb54aa34ee44739
arpitjain97 <46017045+arpitjain97@users.noreply.github.com>
2020-06-24 22:41:02 +0530

    updated

--- arpitjain/script1.js
+++ arpitjain/script1.js
@@ -6,43 +6,43 @@ var command = document.querySelector(".command");
   
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-      if (input.value !="home" || input.value!="skills" || input.value!="about" ||input.value!="help" || input.value!="certificates"){
+      if (input.value.toLowerCase() !="home" || input.value.toLowerCase()!="skills" || input.value.toLowerCase()!="about" ||input.value.toLowerCase()!="help" || input.value.toLowerCase()!="certificates"){
         $("section").hide();
         $('section[id="' + "error" + '"]').show();
         
       }
-   if (input.value == "home"){
+   if (input.value.toLowerCase() == "home"){
     $("section").hide();
-       var destination = input.value;
+       var destination = input.value.toLowerCase();
        console.log("home");
       $('section[id="' + destination + '"]').show();
       $("input").val('');
       
    }
-   if (input.value == "about"){
+   if (input.value.toLowerCase() == "about"){
     $("section").hide();
-    var destination = input.value;
+    var destination = input.value.toLowerCase();
     console.log("about");
     $('section[id="' + destination + '"]').show();
     $("input").val('');
  }
- if (input.value == "skills"){
+ if (input.value.toLowerCase() == "skills"){
     $("section").hide();
-    var destination = input.value;
+    var destination = input.value.toLowerCase();
     console.log("skills");
     $('section[id="' + destination + '"]').show();
     $("input").val('');
  }
- if (input.value == "certificates"){
+ if (input.value.toLowerCase() == "certificates"){
     $("section").hide();
-    var destination = input.value;
+    var destination = input.value.toLowerCase();
     console.log("certificates");
     $('section[id="' + destination + '"]').show();
     $("input").val('');
  }
- if (input.value == "help"){
+ if (input.value.toLowerCase() == "help"){
     $("section").hide();
-     var destination = input.value;
+     var destination = input.value.toLowerCase();
     console.log("help");
     $('section[id="' + destination + '"]').show();
     $("input").val('');
